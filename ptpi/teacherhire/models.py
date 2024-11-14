9ba8e08c69bafae07b0abdedd8d5bc33fb291387
nidhi334 <knidhibharti8178@gmail.com>
2024-11-14 15:48:35 +0530

    Subject model

--- ptpi/teacherhire/models.py
+++ ptpi/teacherhire/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    marks = models.IntegerField()
+    status = models.BooleanField()
+    backlogs = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
