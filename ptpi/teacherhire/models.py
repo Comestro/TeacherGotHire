e925f6c36a52600f3e39bdd4024498b70d81fe0f
Komal Kumari <komalydv2760@gmail.com>
2024-11-14 15:55:06 +0530

    komal work

--- ptpi/teacherhire/models.py
+++ ptpi/teacherhire/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Qualification(models.Model):
+    highest_qualification = models.CharField(max_length=200, null=True, blank=True, choices=(("M.tech", "M.tech"),))
+    institution = models.CharField(max_length=200, null=True, blank=True)
+    board = models.CharField(max_length=50, choices=(('BSEB', 'BSEB'), ('CBSE','CBSE'), ('ICSE', 'ICSE')))
+
+    def __str__(self):
+        return self.highest_qualification
 
 # Create your models here.
+class Teacher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    experience_year = models.IntegerField(null=True, blank=True)
+    qualification = models.ForeignKey(Qualification, on_delete=models.CASCADE, null=True, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Rating(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True)
+    comment = models.CharField(max_length=400, null=True, blank=True)
+
+    def __str__(self):
+        return self.rating
