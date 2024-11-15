82bea86172c24d1cb25474f5d7313966cb40723b
nidhi334 <knidhibharti8178@gmail.com>
2024-11-15 23:55:43 +0530

    all model api added

--- ptpi/teacherhire/serializers.py
+++ ptpi/teacherhire/serializers.py
@@ -1,28 +1,37 @@
 from rest_framework import serializers
-from teacherhire.models import Subject
+from teacherhire.models import Subject, Qualification,Teacher,Rating,Level,Question
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = "__all__"
     
-# class QualificationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Qualification
-#         fields = ['id', 'highest_qualification', 'institution', ]
+class QualificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Qualification
+        fields = "__all__"
 
-# # Create your serializers here.
-# class TeacherSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Teacher
-#         user_name = serializers.CharField(source='user.name')
-#         qualification = serializers.CharField(source='qualification.highest_qualification')
-#         subject = serializers.CharField(source='subject.name')
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        user_name = serializers.CharField(source='user.name')
+        qualification = serializers.CharField(source='qualification.highest_qualification')
+        subject = serializers.CharField(source='subject.name')
 
-#         fields = ['user_name','bio', 'experience_year', 'qualification', 'subject']
+        fields = "__all__"
 
-# class RatingSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Rating
-#         teacher = serializers.CharField(source='teacher.user.name')
-#         fields = ['teacher', 'rating', 'comment']
+class RatingSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Rating
+         teacher = serializers.CharField(source='teacher.user.name')
+         fields = ['teacher', 'rating', 'comment']
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = "__all__"
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
