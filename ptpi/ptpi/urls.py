cedcbe955bbbd78f1ce95e8c330790d9bdb8f789
komalkumari17 <komalydv2760@gmail.com>
2024-11-19 13:33:45 +0530

    add skill api

--- ptpi/ptpi/urls.py
+++ ptpi/ptpi/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
-from teacherhire.views import home, dashboard, manage_teacher, manage_subject,manage_qualification, manage_rating,delete_rating,delete_teacher,delete_quali,delete_subject
+from teacherhire.views import home, dashboard, manage_teacher, manage_subject,manage_qualification, manage_rating,delete_rating,delete_teacher,delete_quali,delete_subject, manage_questions
 
 urlpatterns = [
     # admin panel url
@@ -9,24 +9,27 @@ urlpatterns = [
     path("admin/dashboard/", dashboard, name='admin.dashboard'),
     #Teachers
     path("admin/manage/teacher/", manage_teacher, name='admin.manage.teacher'),
-    path("admin/<int:pk>/delete/", delete_teacher, name='admin.delete.teacher'),
+    path("admin/<int:pk>/delete/teacher/", delete_teacher, name='admin.delete.teacher'),
 
     #Subjects
     path("admin/manage/subject/", manage_subject, name='admin.manage.subject'),
-    path("admin/<int:pk>/delete/", delete_subject, name='admin.delete.subject'),
+    path("admin/<int:pk>/delete/subject/", delete_subject, name='admin.delete.subject'),
 
     #Qualification
     path("admin/manage/qualification/", manage_qualification, name='admin.manage.qualification'),
-    path("admin/<int:pk>/delete/", delete_quali, name='admin.delete.qualification'),
+    path("admin/<int:pk>/delete/qualification/", delete_quali, name='admin.delete.qualification'),
    
     #Rating
     path("admin/manage/rating/", manage_rating, name='admin.manage.rating'),
-    path("admin/<int:pk>/delete/", delete_rating, name='admin.delete.rating'),
+    path("admin/<int:pk>/delete/rating/", delete_rating, name='admin.delete.rating'),
+
+    #Question 
+    path("admin/manage/question/", manage_questions, name='admin.manage.question'),
+
     
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
     path("home/",home),
     path("api/",include('teacherhire.urls'))
-
     
 ]
