79607d116c63fca473f32aa35677905c26f1a353
komalkumari17 <komalydv2760@gmail.com>
2024-11-22 12:53:40 +0530

    changes

--- ptpi/teacherhire/urls.py
+++ ptpi/teacherhire/urls.py
@@ -8,12 +8,13 @@ from rest_framework import routers
 
 
 router = routers.DefaultRouter()
+
 router.register(r'subject',SubjectViewSet)
 router.register(r"qualifications",QualificationViewSet)
 # router.register(r"teachers",TeacherViewSet)
 router.register(r'ratings',RatingViewSet)
 router.register(r'levels',LevelViewSet)
-router.register(r'questions',QuestionViewSet)
+router.register(r'questions',QuestionViewSet)  
 router.register(r'options',OptionViewSet)
 router.register(r'skills',SkillViewSet)
 router.register(r'registers',RegisterViewSet)
